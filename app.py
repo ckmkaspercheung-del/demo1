5af81006d1e04ece349d6a8ff12663ec5904896d
ckmkaspercheung-del <ckmkaspercheung@gmail.com>
2025-11-17 14:53:47 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ st.title("Retail business Dashboard")
 st.header("header")
 st.write("message")
 
-age=st.number_input("Enter monthly Sales Target(in USD):"
+age=st.number_input("Enter monthly Sales Target(in USD):",
                    min_value=0
 				   max_value=100000
 				   value=50000)
