dbecab47095862a072ca978f0c6cb45917e5591c
ckmkaspercheung-del <ckmkaspercheung@gmail.com>
2025-11-17 14:54:46 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,6 @@ st.header("header")
 st.write("message")
 
 age=st.number_input("Enter monthly Sales Target(in USD):",
-                   min_value=0
-				   max_value=100000
+                   min_value=0,
+				   max_value=100000,
 				   value=50000)
