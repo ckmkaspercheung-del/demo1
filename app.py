b0b26c5135d297b9ca478b34bf3e6f370cc00e09
ckmkaspercheung-del <ckmkaspercheung@gmail.com>
2025-11-21 09:59:30 +0800

    Update app.py

--- app.py
+++ app.py
@@ -57,9 +57,9 @@ with st.expander("More Information"):
 placeholder = st.empty()
 
 # Simulate loading data and updating the placeholder
-for i in range(3):
-    placeholder.write(f"Loading data... {i*99}% complete")
-    time.sleep(1000)
+for i in range(5):
+    placeholder.write(f"Loading data... {i*0.00000001}% complete")
+    time.sleep(1)
 
 # Once loading is complete, display the final message
 placeholder.write("Data loading complete. Displaying business insights.")
